f0986e506181fe881d1eb70058f5718edb0e58ae
NelysT <139475672+NelysT@users.noreply.github.com>
2023-09-14 09:46:45 +0000

    proyecto original

--- src/js/layout.js
+++ src/js/layout.js
@@ -10,6 +10,7 @@ import injectContext from "./store/appContext";
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
 
+
 //create your first component
 const Layout = () => {
 	//the basename is used when your project is published in a subdirectory and not in the root of the domain
@@ -26,6 +27,7 @@ const Layout = () => {
 						<Route path="/demo" element={<Demo />} />
 						<Route path="/single/:theid" element={<Single />} />
 						<Route path="*" element={<h1>Not found!</h1>} />
+						
 					</Routes>
 					<Footer />
 				</ScrollToTop>
---
+++ src/js/views/Contact.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Contact = () => {
+
+    return (
+        <div className="container">
+            <nav>
+                <button type="button" className="btn btn-success">Add new contact</button>
+            </nav>
+            <div>
+                <ul>
+                    <
+                </ul>
+            </div>
+        </div>
+    );
+}
+export default Contact
